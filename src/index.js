94c7b61f313185fc2ccd6bcaa4a0d6ae5053bffb
Joel Thoms <jthoms@paciolan.com>
2019-03-06 22:51:17 -0800

    feat: add remote component starter kit files

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import propTypes from "prop-types";
+
+const RemoteComponent = ({ name }) => {
+  return <div>Hello {name}!</div>;
+};
+
+RemoteComponent.propTypes = {
+  name: propTypes.string
+};
+
+RemoteComponent.defaultProps = {
+  name: "World"
+};
+
+export default RemoteComponent;
