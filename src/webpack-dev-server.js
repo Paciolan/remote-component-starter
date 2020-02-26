7d8a022c6750000dabc725146f699c7d727c41c4
Joel Thoms <jthoms@paciolan.com>
2020-02-25 16:25:10 -0800

    feat: ✨ remove private npm packages from repo

--- src/webpack-dev-server.js
+++ src/webpack-dev-server.js
@@ -2,11 +2,9 @@
  * webpack-dev-server entry point for debugging.
  * This file is not bundled with the library during the build process.
  */
-import base, { withTheme } from "@paciolan/pac-theme-base";
 import { RemoteComponent } from "@paciolan/remote-component";
 import React from "react";
 import ReactDOM from "react-dom";
-import { ThemeProvider } from "styled-components";
 import LocalComponent from "./index.js";
 
 // different paths for localhost vs s3
@@ -15,28 +13,15 @@ const url =
 
 const node = document.getElementById("app");
 
-const colors = {
-  orange: "#ffa500"
-};
-
-const theme = withTheme(base).extendWith({
-  colors,
-  typography: {
-    title: {
-      color: colors.orange
-    }
-  }
-});
-
 const Component = props =>
   process.env.NODE_ENV === "development"
     ? <LocalComponent {...props} />
     : <RemoteComponent url={url} {...props} />; // prettier-ignore
 
 const App = () => (
-  <ThemeProvider theme={theme}>
+  <>
     <Component name="Webpack" />
-  </ThemeProvider>
+  </>
 );
 
 ReactDOM.render(<App />, node);
